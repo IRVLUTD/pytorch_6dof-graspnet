bb018967f21b20f667fcfc60fe716607dee231e7
Ninad <ninad.khargonkar@gmail.com>
2023-06-01 19:28:37 -0500

    update params

--- exp_publish_grasps.sh
+++ exp_publish_grasps.sh
@@ -5,4 +5,4 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-python ./ros/test_model_with_segmentation.py --batch_size 100
+python ./ros/test_model_with_segmentation.py --refine_steps 50 --num_grasp_samples 60 --batch_size 30 --choose_fn better_than_threshold_in_sequence 
