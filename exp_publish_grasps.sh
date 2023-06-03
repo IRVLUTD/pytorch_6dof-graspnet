7dc295fffaf1b4dd49a1e9d4466e638876d58811
Ninad <ninad.khargonkar@gmail.com>
2023-06-02 23:10:08 -0500

    better config

--- exp_publish_grasps.sh
+++ exp_publish_grasps.sh
@@ -5,4 +5,4 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-python ./ros/test_model_with_segmentation.py --refine_steps 50 --num_grasp_samples 60 --batch_size 30 --choose_fn better_than_threshold_in_sequence 
+python ./ros/test_model_with_segmentation.py --refine_steps 50 --num_grasp_samples 100 --batch_size 100 --threshold 0.9 --choose_fn better_than_threshold_in_sequence --viz
