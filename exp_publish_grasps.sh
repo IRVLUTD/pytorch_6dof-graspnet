a2f00fa4f853f67ac39648087bdd856510145647
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2023-06-28 11:54:27 -0500

    final experiment params

--- exp_publish_grasps.sh
+++ exp_publish_grasps.sh
@@ -5,4 +5,4 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-python ./ros/test_model_with_segmentation.py --refine_steps 50 --num_grasp_samples 100 --batch_size 100 --threshold 0.9 --choose_fn better_than_threshold_in_sequence --viz
+python ./ros/test_model_with_segmentation.py --refine_steps 25 --num_grasp_samples 80 --batch_size 80 --threshold 0.9 --choose_fn better_than_threshold_in_sequence
